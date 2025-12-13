a30ed80e2bb6ba57f188a534190504587ddd24c7
thanu-12345-r <thanmairayapudi26@gmail.com>
2025-12-13 22:33:27 +0530

    file created

---
+++ Practice Programs/operators.py
@@ -0,0 +1,40 @@
+Python 3.14.1 (tags/v3.14.1:57e0d17, Dec  2 2025, 14:05:07) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=3
+>>> b=4
+>>> a+b
+7
+>>> a-b
+-1
+>>> a*b
+12
+>>> a%b
+3
+>>> a/b
+0.75
+>>> a^b
+7
+>>> a<=b
+True
+>>> a>=b
+False
+>>> a!=2
+True
+>>> a==b
+False
+>>> a<<b
+48
+>>> a in b
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    a in b
+TypeError: argument of type 'int' is not a container or iterable
+>>> 
+>>> a~b
+SyntaxError: invalid syntax
+>>> a<b
+True
+>>> a>b
+False
+>>> a//b
+0
