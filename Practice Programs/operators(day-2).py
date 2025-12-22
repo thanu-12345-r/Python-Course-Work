1d2e1f59e116c2afb4a13a8ccdc4000a012e78b3
thanu-12345-r <thanmairayapudi26@gmail.com>
2025-12-22 19:25:14 +0530

    file created

---
+++ Practice Programs/operators(day-2).py
@@ -0,0 +1,138 @@
+Python 3.14.1 (tags/v3.14.1:57e0d17, Dec  2 2025, 14:05:07) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> b=5
+>>> print(a+b)
+15
+>>> print(a-b)
+5
+>>> print(a*b)
+50
+>>> print(a/b)
+2.0
+>>> print(a%b)
+0
+>>> print(a**b)
+100000
+>>> print(a//b)
+2
+>>> x=5
+>>> x+=2
+>>> print(x)
+7
+>>> x-=2
+>>> print(x)
+5
+>>> x*=2
+>>> print(x)
+10
+>>> x/=2
+>>> print(x)
+5.0
+>>> s=(thanmai,usha,pujitha)
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    s=(thanmai,usha,pujitha)
+NameError: name 'thanmai' is not defined
+>>> s=45
+>>> print(45==45)
+True
+>>> print(!=45)
+SyntaxError: invalid syntax
+print(s!=45)
+False
+print(s<45)
+False
+print(s>45)
+False
+print(s>+45)
+False
+False
+False
+
+print(s>=45)
+True
+print(s<=45)
+True
+name=thanmai
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    name=thanmai
+NameError: name 'thanmai' is not defined
+name"thanmai"
+SyntaxError: invalid syntax
+name="Thanmai"
+print('h in name)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print('h' in name)
+      
+True
+print('v' not in name)
+      
+True
+print('a' in name)
+      
+True
+a=5
+      
+b=10
+      
+print(a$b)
+      
+SyntaxError: invalid syntax
+print(a&b)
+      
+0
+print(a^b)
+      
+15
+print(a~b)
+      
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(a<<b)
+      
+5120
+print(a>>b)
+      
+0
+print(a~b)
+      
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+x=5
+      
+x=10
+      
+print(x>5 and x<10)
+      
+False
+print(x>5 or x<2)
+      
+True
+print(x<5 not x>8)
+      
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(x<5 not x>8)
+      
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print(x<5 not x<6)
+      
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print( x<5 not x<6 )
+      
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+a=(5,10,15)
+      
+b=(5,10,15)
+      
+c=a
+      
+print(a==b)
+      
+True
+print(a is b)
+      
+False
+print(a is c)
+      
+True
