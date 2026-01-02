0c004be19dd5ada5eb42e0061bdd467d68329a88
thanu-12345-r <thanmairayapudi26@gmail.com>
2026-01-02 18:25:42 +0530

    file created

---
+++ Practice Programs/conditional statements(day-5).py
@@ -0,0 +1,54 @@
+num=int(input("Enter a number:"))
+if num>0:
+ print("positive number")
+else:
+ print("Negative number")
+
+
+num=int(input("Enter a number:"))
+if num%2==0:
+    print("Even number")
+else:
+    print("Negative number")
+
+
+age=int(input("Enter your age:"))
+if age>=18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")
+          
+
+num=int(input("Enter a number:"))
+if -100 <= num <=100:
+    print("One digit number:")
+elif -200 <=num <=200:
+    print("Two digit number:")
+else :
+    print("Three digit number:")
+
+
+marks=int(input("Enter your marks:"))
+if marks >=75:
+    print("A grade:")
+elif marks>=60:
+    print("B grade:")
+elif marks>=40:
+    print("C grade:")
+else:
+    print("Fail")
+
+marks=int(input("Enter your marks:"))
+if marks >=75:
+    print("Distinction:")
+elif marks>=60:
+    print("First Class:")
+elif marks>=40:
+     print("Second Class:")
+else:
+    print("Fail")
+
+
+
+    
+    
